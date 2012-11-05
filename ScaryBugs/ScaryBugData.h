c360cbd79c6f7af469dc6906ac066c452f2d27a6
sursrc <sursrc@gmail.com>
2012-11-05 17:49:55 +0800

    first commit

---
+++ ScaryBugs/ScaryBugData.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+@interface ScaryBugData : NSObject
+
+@property (strong) NSString *title;
+@property (assign) float rating;
+
+- (id)initWithTitle:(NSString*)title rating:(float)rating;
+
+@end
---
+++ ScaryBugs/ScaryBugData.m
@@ -0,0 +1,16 @@
+#import "ScaryBugData.h"
+
+@implementation ScaryBugData
+
+@synthesize title = _title;
+@synthesize rating = _rating;
+
+- (id)initWithTitle:(NSString*)title rating:(float)rating {
+    if ((self = [super init])) {
+        self.title = title;
+        self.rating = rating;
+    }
+    return self;
+}
+
+@end
